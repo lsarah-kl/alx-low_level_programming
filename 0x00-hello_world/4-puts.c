c92041249584d6e83587d849d8055e3459cc71bf
Sarah <sarah.t.karim@gmail.com>
2023-04-20 08:47:52 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 /**
  * main - Entry point
- *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function
- *
  * Return: Always 0 (Success)
  */
 
@@ -12,6 +8,5 @@
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
-
     return (0);
 }
