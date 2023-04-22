aa774bfae3f37094cde65f85f9d29df73ab6cc70
Sarah <sarah.t.karim@gmail.com>
2023-04-21 17:15:04 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+/* Print lowercase alphabet */
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+/* Print uppercase alphabet */
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+
+/* Print newline character */
+	putchar('\n');
+
+	return (0);
+}
