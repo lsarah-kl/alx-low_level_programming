f17869219ed46d45a3fb02d517fb97395b1425dc
Sarah <sarah.t.karim@gmail.com>
2023-05-10 17:48:31 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of number n
+ * @n: number n
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
+
