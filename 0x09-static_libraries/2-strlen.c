bbfbebfac6e373d94bbb5475e3ba973a1fa721ce
Sarah <sarah.t.karim@gmail.com>
2023-05-12 19:07:30 -0700

    libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+2-strlen.c
+
+#include "main.h"
+/**
+ * _strlen - returns the len of a str
+ * @s: the string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absolute value of an integer
+ * @n: int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
